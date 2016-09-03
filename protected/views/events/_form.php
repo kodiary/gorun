59cbb82662a134559b61fd8eddeb0eef1aff58d4
Anwar <justdoit2045@gmail.com>
2016-09-03 18:04:57 +0545

    changes

--- protected/views/events/_form.php
+++ protected/views/events/_form.php
@@ -1,4 +1,6 @@
 <script src="//cdn.ckeditor.com/4.5.10/basic/ckeditor.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_Gjdm_0nJk17UVBPoV5Im40uQeguoRAo"></script>
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/gmap.js"></script>
 <form action="<?php echo Yii::app()->request->baseUrl;?>/dashboard" id="profile-detail" method="post">
                 <div class="form-group white">
                     <label class="col-md-12">Event title</label>
@@ -48,19 +50,130 @@
                 <div class="loadform">
                     
                 </div>
-                <div class="form-group">
-                    <label class="col-md-2">Event Times<span class="required">*</span></label>
-                    <div class="col-md-9"><input type="checkbox" class="include_times" name="include_times" value="1" /> &nbsp; Include Times</div>
+                <div class="form-group white">
+                    <label class="col-md-12">Event Info<br /><span class="blue">All the details about your event</span></label>
+                    
+                    <div class="col-md-12"><textarea class="form-control description" name="description"><?php echo $model->description;?></textarea></div>
                     <div class="clearfix"></div>
                 </div>
-                <div class="load_time" style="display: none;"></div>
-                <hr />
-                <div class="form-group">
-                    <label class="col-md-11">Event Description<span class="blue"> - Provide the description of the event - try to keep it short</span></label>
-                    <div class="col-md-11"><textarea class="form-control description" name="description"><?php echo $model->description;?></textarea></div>
+                <div class="form-group white">
+                    <label class="col-md-12">Cover image (O<span style="text-transform: lowercase;">ptional</span>)</label>
+                    <div class="col-md-12 profilepic">
+                    <div class="profile_img event_img" id="upimage_0">
+                    <?php
+                    /*if($member->logo && (Yii::app()->basePath.'/../images/frontend/thumb/'.$member->logo))
+                    {
+                        $img_url=Yii::app()->baseUrl.'/images/frontend/thumb/'.$member->logo;
+                        echo '<img src="'.$img_url.'"/>';
+                    }*/
+                    
+                    ?>
+                        
+                    </div>
+                    <div class="col-md-5 picact">
+                    <?php echo $this->renderPartial('application.views.gallery._addImage',array('member'=>$model,'type'=>'event')); ?>
+                        
+                      
+                <?php
+                        
+                //crop button
+                echo CHtml::ajaxLink('<span class="fa fa-crop"></span> Crop',
+                        $this->createUrl('gallery/cropPhoto?height=215&width=215'),
+                         array( //ajax options
+                         'data'=>array('fileName'=>"js:function(){ return $('.uploaded_image').val()}",'id'=>$member->id),
+                         'type'=>'POST',
+                        'success'=>"js:function(data){
+                                    if(data!=''){
+                                        $('#cropModal').html(data).dialog('open');
+                                        $('.ui-dialog-titlebar-close').hide();
+                                         return false;
+                                    }
+                                    else
+                                        alert('No Image selected');
+                                    }",
+                        'complete'=>"js:function(){
+                                      $('#crop_".$member->id."').val('<span class=\"fa fa-crop\"></span> Crop');
+                                    }",
+                        ),
+                        array('id'=>'crop_'.$member->id,'class'=>'btn btn-crop','onclick'=>'$("#crop_'.$member->id.'").val("loading...");')//html options
+                );
+                ?><br />
+                
+                        
+                        <a href="javascript:void(0)" class="btn btn-remove" onclick="return confirm_delete('Are you sure that you want to remove the image?'); "><span class="fa fa-times" style="color: #E00000;"></span> Remove</a><br />
+                    </div>
+                        
+                    </div>
                     <div class="clearfix"></div>
                 </div>
-                <hr />
+                
+                <div class="form-group white">
+                    <label class="col-md-12">Online Entry Link (Optional)<br /><span class="blue">Post your online entry link here if you have one. Will display as button on event page.</span></label>
+                    
+                    <div class="col-md-12"><input type="text" class="form-control" placeholder="http://www..." name="online_entry" value="<?php echo $model->online_entry;?>" /></div>
+                    <div class="clearfix"></div>
+                </div>
+                
+                <div class="form-group white">
+                    <label class="col-md-12">Event Location (Required)<br /><span class="blue">Input the venue name or address below to find it on google map. Drag pin to desired located if required.</span></label>
+                    
+                    <div class="col-md-12"><input type="text" onblur='codeAddress()' id="formattedAddress" class="form-control venue" placeholder="Enter venue name or street address" name="venue" value="<?php echo $model->venue;?>" /></div>
+                    <div class="clearfix"></div>
+                    <div class="sn_group" style="display: none;">
+                        	<div class="s1"><input id="Company_latitude" value="" type="text" name="latitude" onchange='updateMapPinPosition()' /></div>
+                            <div class="s2"><input id="Company_longitude" value="" type="text" name="longitude" onchange='updateMapPinPosition()' /></div>
+                            <div class="clear"></div>
+                         </div>
+                    <div id="map_canvas" style="height: 200px;background:#e5e5e5;margin-top:15px;"></div>
+                    
+                </div>
+                
+                <div class="form-group white">
+                    <label class="col-md-12">Event Organizer (Optional)<br /><span class="blue">Displayed contact details of the event organizer.</span></label>
+                    <div class="org_group">
+                        <div class="col-md-12">Organizer Name</div>
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Organizer Name" name="org_name" value="<?php echo $model->online_entry;?>" />
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="org_group">
+                        <div class="col-md-12">Contact Number</div>
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Contact Number" name="org_contact" value="<?php echo $model->online_entry;?>" />
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="org_group">
+                        <div class="col-md-12">Email address (Will display a contact button)</div>
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Contact Email" name="org_email" value="<?php echo $model->online_entry;?>" />
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="org_group">
+                        <div class="col-md-12">Website address</div>
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Organizer website address" name="org_website" value="<?php echo $model->online_entry;?>" />
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                
+                <div class="form-group white">
+                    <label class="col-md-12">Visibility<br /><span class="blue">Events are subject to approval before going live and visible to public.</span></label>
+                    <div class="clearfix"></div>
+                </div>
+                
+                <div class="submit_group">
+                    <div class="col-md-6">
+                        <strong>PLEASE NOTE: Submitting and event requires our approval before it will be displayed live.</strong>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="submit" class="btn btn-submit" value="SUBMIT FOR APPROVAL" />
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
                 
                 
                 <?php /*
@@ -245,12 +358,73 @@
                 
                 <hr />
                 <?php */?>
-                <div class="form-group">
-                <input type="submit" name="submit" value="Save Changes" class="btn btn-default bgblue btn-lg" />
-                </div>
+                
                 
             </form>
-
+<script>
+$(function(){
+    initialize();
+    CKEDITOR.replace( 'description' );
+    CKEDITOR.editorConfig = function( config ) {
+	config.language = 'es';
+	config.uiColor = '#F5f5f5';
+	
+};
+    /*$('.add_distance').click(function(){
+        
+       var distance = $('.dist').val();
+       var distance_decimal = $('.distance_decimal').val();
+       var start_hour = $('.start_hour').val();
+       var start_min = $('.start_min').val();
+       var cost = $('.cost').val();
+       if((!distance && !distance_decimal))
+       {
+        $('.dist').attr('style','border-color: #a94442;');
+        return false;
+       }
+       else
+       {
+        $('.dist').removeAttr('style');
+        
+       }
+       if(!start_hour && !start_min)
+        {
+            $('.start_hour').attr('style','border-color: #a94442;');
+             $('.start_min').attr('style','border-color: #a94442;');
+             return false;
+        }
+        else
+        {
+            $('.start_hour').removeAttr('style');
+            $('.start_min').removeAttr('style');
+        }
+        var dist = distance;
+        if(distance=='')
+        {
+            distance = '0.'+distance_decimal;
+        }
+        if(start_hour=='')
+        start_hour='00';
+        if(start_min=='')
+        start_min ='00';
+        if(cost=='')
+        cost = 'n/a';
+        cost = 'R'+cost;
+        var st = distance;
+        var st = st+'km &nbsp;|&nbsp; <span class="blue">Start</span> - '+start_hour+':'+start_min+' &nbsp;|&nbsp; <span class="blue">Cost</span> - '+cost;
+        $('.distance_list').show();
+        $('.distance_list').append(
+        '<li>'+st+'<a href="javascript:void(0)" class="delete_distance" onclick="$(this).closest(\'li\').remove();"><b class="fa fa-times"></b></a></li>'+
+        '<input type="hidden" name="distance[]" value="'+dist+'" /><input type="hidden" name="distance_decimal[]" value="'+distance_decimal+'" /><input type="hidden" name="start_hour[]" value="'+start_hour+'" /><input type="hidden" name="start_min[]" value="'+start_min+'" /><input type="hidden" name="cost[]" value="'+cost.replace('R','')+'" />'
+        );
+       
+    });*/
+    
+})
+</script>
+<style>
+.btn-black{padding:5px;font-size:20px;text-transform: uppercase;background:#FFF!important;width:200px!important;color:#000;}
+</style>
 <script>
   function validate_date(s,e)
   {
