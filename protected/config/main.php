eba2fd7b2516737a60403de0769adb7f973afb19
Anwar <justdoit2045@gmail.com>
2016-11-09 12:00:04 +0545

    changes

--- protected/config/main.php
+++ protected/config/main.php
@@ -192,7 +192,11 @@ return array(
             'events/loadTime'=>'events/loadTime',
             
             'dashboard'=>'dashboard/index',
+            'run'=>'run/index',
+            'bike'=>'bike/index',
+            'triathlon'=>'triathlon/index',
             '<slug>'=>'dashboard/details/slug/<slug>',
+            
             '<controller:\w+>/<id:\d+>'=>'<controller>/view',
 			'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 			'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
