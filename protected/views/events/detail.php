8662f8ba9816277ed0e88c55a4faff3fd7b93f77
Anwar <justdoit2045@gmail.com>
2016-11-09 12:01:31 +0545

    changes

--- protected/views/events/detail.php
+++ protected/views/events/detail.php
@@ -174,7 +174,7 @@
     <div class="modal-content">
       <div class="modal-header bluebg">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Your Result</h4>
+        <h4 class="modal-title" id="myModalLabel">YOUR RESULTS</h4>
       </div>
       <div class="modal-body" style="position: relative;padding-bottom:115px;">
         <!-- Normal -->      
