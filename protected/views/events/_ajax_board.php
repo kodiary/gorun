d0648ae2c70de09f143aec1b42fe1018dc6a6035
Anwar <justdoit2045@gmail.com>
2016-12-08 12:11:07 +0545

    changes

--- protected/views/events/_ajax_board.php
+++ protected/views/events/_ajax_board.php
@@ -12,7 +12,7 @@ if(count($result))
         $mem = Member::model()->findByPk($r->user_id);
         $i++;
         ?>
-        <tr><td><?php echo $i;?></td><td><?php echo $mem->fname.' '.$mem->lname;?></td><td><?php if($r->distance_tri){echo $r->distance_tri;}else echo str_replace('.',',',$r->distance);?>km </td><td>...</td><td><?php echo $r->dist_time;?></td></tr>
+        <tr><td><?php echo $i;?></td><td><a href="<?php echo Yii::app()->request->baseUrl;?>/<?php echo $mem->username;?>"><?php echo $mem->fname.' '.$mem->lname;?></a></td><td><?php if($r->distance_tri){echo $r->distance_tri;}else echo str_replace('.',',',$r->distance);?>km </td><td>...</td><td><?php echo $r->dist_time;?></td></tr>
         <?php
     }
     ?>
