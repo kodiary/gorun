99c4f0695a0e3a7e3f4ae58aa3daf9510ed80e46
Anwar Ali <info@kodiary.com>
2017-09-23 00:05:35 +0545

    changes

--- protected/config/main.php
+++ protected/config/main.php
@@ -296,9 +296,9 @@ return array(
 				),
 				// uncomment the following to show log messages on web pages
 			
-				array(
+				/*array(
 					'class'=>'CWebLogRoute',
-				),
+				),*/
 				
 			),
 		),
